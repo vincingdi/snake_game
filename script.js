1cfc5bdcee2d3f6d0881d04d49b2000c4b1cd583
vincingdi <110591050+vincingdi@users.noreply.github.com>
2025-11-28 20:49:38 +0200

    Snake lenghtening functionality

--- script.js
+++ script.js
@@ -54,22 +54,22 @@ function move(grid, current_pos, direction) {
     var pos_1 = current_pos.pos1, pos_2 = current_pos.pos2;
     if (direction.current_direction === 'Down') {
         pos_1++;
-        grid[pos_1][pos_2].style.backgroundColor = "black";
-        grid[pos_1-1][pos_2].style.backgroundColor = "gray";
+        // grid[pos_1][pos_2].style.backgroundColor = "black";
+        // grid[pos_1-1][pos_2].style.backgroundColor = "gray";
     }
     else if (direction.current_direction === 'Up') {
         pos_1--;
-        grid[pos_1][pos_2].style.backgroundColor = "black";
-        grid[pos_1+1][pos_2].style.backgroundColor = "gray";
+        // grid[pos_1][pos_2].style.backgroundColor = "black";
+        // grid[pos_1+1][pos_2].style.backgroundColor = "gray";
     }
     else if (direction.current_direction === 'Left') {
         pos_2--;
-        grid[pos_1][pos_2].style.backgroundColor = "black";
-        grid[pos_1][pos_2+1].style.backgroundColor = "gray";
+        // grid[pos_1][pos_2].style.backgroundColor = "black";
+        // grid[pos_1][pos_2+1].style.backgroundColor = "gray";
     } else if (direction.current_direction === 'Right') {
         pos_2++;
-        grid[pos_1][pos_2].style.backgroundColor = "black";
-        grid[pos_1][pos_2-1].style.backgroundColor = "gray";
+        // grid[pos_1][pos_2].style.backgroundColor = "black";
+        // grid[pos_1][pos_2-1].style.backgroundColor = "gray";
     }
 
     current_pos.pos1 = pos_1;
@@ -104,27 +104,79 @@ function changeDirection(direction_object) {
 }
 
 
-function createFood(squaresPerSide, grid, foodPos) {
+function createFood(squaresPerSide, foodPos) {
     let rowN = Math.floor(Math.random() * squaresPerSide);
     let columnN = Math.floor(Math.random() * squaresPerSide);
-    grid[rowN][columnN].style.backgroundColor = "green";
     foodPos.pos1 = rowN;
     foodPos.pos2 = columnN;
     
 }
 
+
+function boardClear(grid, squaresPerSide) {
+    for (let i=0; i<squaresPerSide; i++) {
+        for (let j=0; j<squaresPerSide; j++) {
+            grid[i][j].style.backgroundColor = "gray";
+        }
+    }
+}
+
+function draw(snake_seg, seg_count, grid, foodPos) {
+    grid[foodPos.pos1][foodPos.pos2].style.backgroundColor = "green";
+    for (let i=0; i<seg_count; i++) {
+        grid[snake_seg[i][0]][snake_seg[i][1]].style.backgroundColor = "black";
+    }
+
+}
+
+
 function mainLoop() {
     squaresPerSide = 16;
+    seg_count = 1;
     grid = createGrid(squaresPerSide);
-    grid[0][0].style.backgroundColor = "black";
+    snake_seg = [[currentPos.pos1, currentPos.pos2]]
+    let x1, x2, y1, y2;
     window.setInterval(function() {
-        move(grid, currentPos, direction);
+        move(grid, currentPos, direction, snake_seg);
+        for (let i=1; i<seg_count; i++) {
+            x1 = snake_seg[i][0];
+            x2 = snake_seg[i][1];
+            console.log(x1, x2);
+
+            if (i == 1) {
+                snake_seg[i][0] = snake_seg[i-1][0];
+                snake_seg[i][1] = snake_seg[i-1][1];
+            } else {
+                snake_seg[i][0] = y1;
+                snake_seg[i][1] = y2;
+            }
+            y1 = x1;
+            y2 = x2
+        } 
+
+        snake_seg[0][0] =  currentPos.pos1;
+        snake_seg[0][1] =  currentPos.pos2;
+        boardClear(grid, squaresPerSide);
+        draw(snake_seg, seg_count, grid, foodPos)
         if (currentPos.pos1 == foodPos.pos1 && currentPos.pos2 == foodPos.pos2) {
-            createFood(squaresPerSide, grid, foodPos);
+            if (direction.current_direction == 'Down') {
+               snake_seg.push([snake_seg[seg_count-1][0]-1, snake_seg[seg_count-1][1]]) 
+            } 
+            if (direction.current_direction == 'Up') {
+               snake_seg.push([snake_seg[seg_count-1][0]+1, snake_seg[seg_count-1][1]]) 
+            } 
+            if (direction.current_direction == 'Right') {
+               snake_seg.push([snake_seg[seg_count-1][0], snake_seg[seg_count-1][1]+1]) 
+            } 
+            if (direction.current_direction == 'Left') {
+               snake_seg.push([snake_seg[seg_count-1][0]-1, snake_seg[seg_count-1][1]-1]) 
+            } 
+            seg_count++;
+            createFood(squaresPerSide, foodPos);
         }
     }, 250);
     changeDirection(direction);
-    createFood(squaresPerSide, grid, foodPos);
+    createFood(squaresPerSide, foodPos);
 }
 
 
